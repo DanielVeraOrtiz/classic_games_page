e84c48829e74948422b0857503dc11b0c994a021
Daniel <di.vera@uc.cl>
2025-12-02 22:03:09 -0300

    Se termina la navbar y sidebar

--- src/router.jsx
+++ src/router.jsx
@@ -1,9 +1,30 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+import Navbar from './components/navbar/navbar';
+import Sidebar from './components/sidebar/sidebar';
+
+function Layout() {
+    const [isOpen, setIsOpen] = useState(window.innerWidth >= 768 ? true : false);
+    return (
+        <>
+            <Navbar isOpen={isOpen} setIsOpen={setIsOpen} />
+            <div className='layout'>
+                <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+                <main>
+                    <Outlet />
+                </main>
+            </div>
+        </>
+    );
+}
 
 export default function Router() {
+
     return (
         <BrowserRouter>
             <Routes>
+                <Route path='/' element={<Layout />}>
+                </Route>
             </Routes>
         </BrowserRouter>
     );
